91f1d94807697d5642b1fa5cec53358691e09880
alina <alina_2000@hotmail.co.uk>
2023-05-25 15:27:11 +0100

    readme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'))
 root.render(
+    <React.StrictMode>
     <Router>
         <App/>
     </Router>
+    </React.StrictMode>
 )
